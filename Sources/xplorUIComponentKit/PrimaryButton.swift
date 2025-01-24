33f7c87b94db913be903457dfda25333ed51656d
GarimaSaini1994 <garima.saini@xplortechnologies.com>
2025-01-24 23:18:14 +0530

    public init is required.

--- Sources/xplorUIComponentKit/PrimaryButton.swift
+++ Sources/xplorUIComponentKit/PrimaryButton.swift
@@ -10,40 +10,25 @@ import Foundation
 import SwiftUI
 
 public struct PrimaryButton: View {
-//    let title: String
-//    let action: () -> Void
-//
-//    // Custom initializer to accept the required parameters
-//    public init(title: String, action: @escaping () -> Void) {
-//        self.title = title
-//        self.action = action
-//    }
-//
-//    public var body: some View {
-//        Button(action: {
-//            action()
-//        }) {
-//            Text(title)
-//                .padding()
-//                .background(Color.blue)
-//                .foregroundColor(.white)
-//                .cornerRadius(5)
-//        }
-//    }
-    
-    public let title: String
-    public     let action: () -> Void
+    let title: String
+    let action: () -> Void
 
-        public var body: some View {
-            Button(action: {
-                action() // Trigger the closure when button is clicked
-            }) {
-                Text(title)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
+    // Custom initializer to accept the required parameters
+    public init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+
+    public var body: some View {
+        Button(action: {
+            action()
+        }) {
+            Text(title)
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(5)
         }
+    }
 }
 
