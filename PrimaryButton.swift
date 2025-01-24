d1fcf7f009cd31fd0945208cd19834cbd612fbd1
GarimaSaini1994 <garima.saini@xplortechnologies.com>
2025-01-24 22:48:32 +0530

    changed init.

--- PrimaryButton.swift
+++ PrimaryButton.swift
@@ -7,16 +7,22 @@
 
 import Foundation
 
+import SwiftUI
+
 public struct PrimaryButton: View {
     let title: String
     let action: () -> Void
-    public init() {}
-    
+
+    // Custom initializer to accept the required parameters
+    public init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+
     public var body: some View {
         Button(action: {
             action()
-        })
-        {
+        }) {
             Text(title)
                 .padding()
                 .background(Color.blue)
@@ -25,3 +31,4 @@ public struct PrimaryButton: View {
         }
     }
 }
+
