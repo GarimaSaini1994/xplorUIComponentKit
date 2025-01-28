45d62b47332302573ecd2020ae02baad77f0aec8
GarimaSaini1994 <garima.saini@xplortechnologies.com>
2025-01-28 18:29:45 +0530

    Added.

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Garima Saini on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/ExampleTests/ExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTests.swift
+//  ExampleTests
+//
+//  Created by Garima Saini on 28/01/25.
+//
+
+import Testing
+@testable import Example
+
+struct ExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
